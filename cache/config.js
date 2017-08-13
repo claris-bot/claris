d061dc4634565d22d3356bdc3bf988e7e0d4525c
Reedzev_ <mikolaj1440@hotmail.com>
2017-08-13 18:57:25 +0200

    Added default config

---
+++ cache/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    token: "xxxxxxxxxxxxxxxxxxxxxxxx.xxxxxx.xxxxxxxxxxxxxxxxxxxxxxxxxxx" //DISCORD TOKEN,
+    prefix: "[" //COMMAND PREFIX,
+    defaultEmbedColor: "#ffffff" //DEFAULT EMBED COLOR
+};
